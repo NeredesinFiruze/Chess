68e728eb8e257c28ed509752ba355339abf72f17
NeredesinFiruze <nahya5601@gmail.com>
2023-02-09 17:25:22 +0300

    coordinate system is complete

--- app/src/main/java/com/example/chess/chess_board/UIBoard.kt
+++ app/src/main/java/com/example/chess/chess_board/UIBoard.kt
@@ -5,25 +5,42 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+
 @Composable
 fun UIBoard() {
-    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-        repeat(4){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        repeat(4) { col ->
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                repeat(4){
-                    CellGray()
-                    CellBlack()
+                repeat(4) { row ->
+                    CellGray(
+                        col = 9 - ((col + 1) * 2 - 1),
+                        row = (row + 1) * 2 - 1
+                    )
+                    CellBlack(
+                        col = 9 -((col + 1) * 2 - 1),
+                        row = (row + 1) * 2
+                    )
                 }
             }
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                repeat(4){
-                    CellBlack()
-                    CellGray()
+                repeat(4) { row ->
+                    CellBlack(
+                        col = 9 -((col + 1) * 2),
+                        row = (row + 1) * 2 - 1
+                    )
+                    CellGray(
+                        col =9 - ((col + 1) * 2),
+                        row = (row + 1) * 2
+                    )
                 }
             }
         }
@@ -33,16 +50,19 @@ fun UIBoard() {
 fun CellGray(
     size: Dp = 45.dp,
     isClicked: Boolean = false,
+    col: Int,
+    row: Int,
     content: @Composable BoxScope.() -> Unit = {}
 ) {
-    var click  by remember { mutableStateOf(!isClicked) }
+    var click by remember { mutableStateOf(!isClicked) }
 
     Box(
         modifier = Modifier
             .background(if (click) Color.Gray else Color.Blue)
-            .size(DpSize(size,size))
+            .size(DpSize(size, size))
             .clickable {
                 click = !click
+                println("col: $col, row: $row")
             },
         content = content
     )
@@ -51,16 +71,19 @@ fun CellGray(
 fun CellBlack(
     size: Dp = 45.dp,
     isClicked: Boolean = false,
+    col: Int,
+    row: Int,
     content: @Composable BoxScope.() -> Unit = {}
 ) {
-    var click  by remember { mutableStateOf(!isClicked) }
+    var click by remember { mutableStateOf(!isClicked) }
 
     Box(
         modifier = Modifier
             .background(if (click) Color.Black else Color.Blue)
-            .size(DpSize(size,size))
+            .size(DpSize(size, size))
             .clickable {
                 click = !click
+                println("col: $col, row: $row")
             },
         content = content
     )
